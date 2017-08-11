4902ed85a31aa9b5f1311d1d320c9a8982da9cbc
altern <sergey.shmarkatyuk@gmail.com>
2017-08-11 21:38:01 +0300

    uncommented and adjusted next bunch of tests

--- Util.hs
+++ Util.hs
@@ -2,6 +2,7 @@ module Util where
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as LBS
+import Control.Lens
 
 lazyToStrictBS :: LBS.ByteString -> BS.ByteString
 lazyToStrictBS x = BS.concat $ LBS.toChunks x
@@ -11,8 +12,10 @@ strictBStoLazy x = LBS.fromStrict x
 
 {-replaceNth n newVal (x:xs)-}
      {-| n == 0 = newVal:xs-}
+     {-| n > (length (x:xs)) = (x:xs)-}
      {-| otherwise = x:replaceNth (n-1) newVal xs-}
-replaceNth n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
+{-replaceNth n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls-}
+replaceNth n newval xs = (element n .~ newval) xs
 
 lastN :: Int -> [a] -> [a]
 lastN n xs = let m = length xs in drop (m-n) xs
